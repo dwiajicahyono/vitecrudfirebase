018d98f779498eed5910bcf324f604c7a295725b
Dwi Aji Cahyono <dwiaji724@gmail.com>
2023-08-20 16:04:48 +0700

    add feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,110 +6,95 @@ const Header = () => {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className="w-full bg-blue-500 shadow fixed">
-      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
-        <div>
-          <div className="flex items-center justify-between py-3 md:py-2 md:block">
-            <Link to="/">
-              <h2 className="text-2xl font-bold text-white">Inventory Telti</h2>
-            </Link>
-            <div className="md:hidden">
-              <button
-                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
-                onClick={() => setNavbar(!navbar)}
-              >
-                {navbar ? (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
-          </div>
-        </div>
-        <div>
-          <div
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-              navbar ? "block" : "hidden"
-            }`}
+    <nav className="w-full bg-blue-500 shadow fixed py-0">
+    <div className="justify-between px-4 mx-auto lg:max-w-7xl md:flex md:px-8 py-0">
+      <div className="flex items-center justify-between py-0 md:block">
+        <Link to="/" className="text-base font-bold text-white py-0">
+          <h2 className=" text-base font-bold text-white py-0">Inventory Telti</h2>
+        </Link>
+        <div className="md:hidden">
+          <button
+            className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+            onClick={() => setNavbar(!navbar)}
           >
-            <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0 md:pt-9 pt-0 text-center">
-              <li>
-                <Link
-                  to="/"
-                  className="text-white hover:text-indigo-200 text-2xl md:text-3xl"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/about"
-                  className="text-white hover:text-indigo-200 text-2xl md:text-3xl"
-                >
-                  About
-                </Link>
-              </li>
-              <div className="mt-3 space-y-2 md:hidden inline-block">
-              <Link
-                to="/admin"
-                className="inline-block w-full px-4 py-2 text-center text-white bg-green-500 rounded-md shadow hover:bg-yellow-500 hover:text-white"
+            {navbar ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 text-white"
+                viewBox="0 0 20 20"
+                fill="currentColor"
               >
-                Sign in
-              </Link>
-              <Link
-                to="/register"
-                className="inline-block w-full px-4 py-2 text-center text-white bg-red-500 rounded-md shadow hover:bg-yellow-500 hover:text-white"
+              <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 text-white"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
               >
-                Sign up
-              </Link>
-            </div>
-            
-              </ul>
-
-            
-          </div>
-        </div>
-        <div className="hidden space-x-2 md:inline-block ">
-          <Link
-            to="/admin"
-            className="px-4 py-2 text-white bg-green-500 rounded-md shadow  hover:bg-yellow-500 hover:text-white"
-          >
-            Login
-          </Link>
-          <Link
-            to="/register"
-            className="px-4 py-2 text-white bg-red-500 rounded-md shadow  hover:bg-yellow-500 hover:text-white"
-          >
-            Register
-          </Link>
+              <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+              </svg>
+            )}
+          </button>
         </div>
       </div>
-    </nav>
+      <div className={`flex-1 pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"}`}>
+        <ul className="flex flex-col items-center justify-center space-y-8 md:flex-row md:space-x-6 md:space-y-0 md:pt-0 text-center h-full">
+          <li>
+            <Link to="/" className="text-white hover:text-indigo-200 text-2xl md:text-xl">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="text-white hover:text-indigo-200 text-2xl md:text-xl">
+              About
+            </Link>
+          </li>
+          <div className="mt-3 space-y-2 md:hidden inline-block">
+          <Link
+          to="/admin"
+          className="inline-block w-full px-4 py-2 text-center text-white bg-green-500 rounded-md shadow hover:bg-yellow-500 hover:text-white"
+        >
+          Sign in
+        </Link>
+        <Link
+          to="/register"
+          className="inline-block w-full px-4 py-2 text-center text-white bg-red-500 rounded-md shadow hover:bg-yellow-500 hover:text-white"
+        >
+          Sign up
+        </Link>
+          </div>
+        </ul>
+      </div>
+      <div className="hidden md:flex md:flex-row md:items-center space-x-2">
+        <Link
+          to="/admin"
+          className="px-4 py-2 text-white bg-green-500 rounded-md shadow hover:bg-yellow-500 hover:text-white"
+        >
+          Login
+        </Link>
+        <Link
+          to="/register"
+          className="px-4 py-2 text-white bg-red-500 rounded-md shadow hover:bg-yellow-500 hover:text-white"
+        >
+          Register
+        </Link>
+      </div>
+    </div>
+  </nav>
+  
+
   );
 };
 
